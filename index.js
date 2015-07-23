81dd23826a96c0a03265537f60dfee7d1990fc25
Marcos Bérgamo <markusdarkness@gmail.com>
2015-07-23 08:23:14 -0300

    Calculate similarity of two articles.

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
+var Promise = require('bluebird');
 var natural = require('natural'); // import framework
+var Diffbot = require('diffbot-api-client');
 
-var quote = 'Node.js are good to create ours api';
-var quote2 = 'In a beatiful day my little poney has begun!';
-var quote3 = 'Addons can be create in C/C++ to run in Node.js';
-var similarity = natural.JaroWinklerDistance(quote, quote3); 
+var diffbot = new Diffbot({token: process.env.DIFFBOT_TOKEN});
+var article = 'http://blog.modulus.io/writing-your-first-npm-module'; //'http://matt-harrison.com/moving-from-express-to-hapi-js/';
+var article2 = 'https://nodesource.com/blog/your-first-nodejs-package'; //'https://www.airpair.com/node.js/posts/nodejs-framework-comparison-express-koa-hapi';
 
+var retArticle = function(link){
+	return new Promise(function(resolve, reject){
+		diffbot.analyze({url: link}, function(err, request, result){
+			if(err){
+				return reject(err);
+			}
+			
+			result = JSON.parse(result);
+			return resolve(result.objects[0].text);
+		});
+	});
+}
+
+Promise.props({
+	article: retArticle(article),
+	article2: retArticle(article2)
+})
+.then(function(results){
+//var similarity = natural.JaroWinklerDistance(quote, quote3); 
+	var similarity = natural.JaroWinklerDistance(results.article, results.article2);
+// classification heuristic more than 50% similar
+	if(similarity >= 0.5){
+		console.log('Text are similar or about the same things');
+	}else{
+		console.log('Text aren\'t similar!'); 
+	}
+	console.log('similarity: '+similarity);
+});
 
 // classification heuristic more than 58% similar
-if(similarity >= 0.58){
-	console.log('Text are similar or about the same things');
-}else{
-	console.log('Text aren\'t similar!'); 
-}
-console.log('similarity: '+similarity);
+// if(similarity >= 0.58){
+// 	console.log('Text are similar or about the same things');
+// }else{
+// 	console.log('Text aren\'t similar!'); 
+// }
+// console.log('similarity: '+similarity);
